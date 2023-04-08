f9767767fe5712eb84640dca72d3c7e4046126f2
Jeisson Montenegro <soyjeison2017@gmail.com>
2023-04-08 12:38:08 -0500

    Drag and drop dinamic

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,6 +1,6 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
-import { ToDo } from 'src/app/models/todo.models';
+import { Column, ToDo } from 'src/app/models/todo.models';
 
 @Component({
   selector: 'app-board',
@@ -10,34 +10,70 @@ import { ToDo } from 'src/app/models/todo.models';
 export class BoardComponent {
 
 
-  todos: ToDo[] = [
+  columns: Column[] = [
     {
-      id: '1',
-      title: 'Make dishes'
+      title: 'ToDo',
+      todos: [
+        {
+          id: '1',
+          title: 'Make dishes'
+        },
+        {
+          id: '2',
+          title: 'Buy a unicorn'
+        },
+      ]
     },
     {
-      id: '2',
-      title: 'Buy a unicorn'
+      title: 'Doing',
+      todos: [
+        {
+          id: '3',
+          title: 'Watch Angular Path in Platzi'
+        }
+      ]
     },
     {
-      id: '3',
-      title: 'Watch Angular Path in Platzi'
+      title: 'Done',
+      todos: [
+        {
+          id: '3',
+          title: 'Play video games'
+        }
+      ]
     }
   ]
 
 
-  doing: ToDo[] = [
-    {
-      id: '3',
-      title: 'Watch Angular Path in Platzi'
-    }
-  ]
-  done: ToDo[] = [
-    {
-      id: '3',
-      title: 'Play video games'
-    }
-  ]
+
+  // todos: ToDo[] = [
+  //   {
+  //     id: '1',
+  //     title: 'Make dishes'
+  //   },
+  //   {
+  //     id: '2',
+  //     title: 'Buy a unicorn'
+  //   },
+  //   {
+  //     id: '3',
+  //     title: 'Watch Angular Path in Platzi'
+  //   }
+  // ]
+
+
+  // doing: ToDo[] = [
+  //   {
+  //     id: '3',
+  //     title: 'Watch Angular Path in Platzi'
+  //   }
+  // ]
+  // done: ToDo[] = [
+  //   {
+  //     id: '3',
+  //     title: 'Play video games'
+  //   }
+  // ]
 
   drop($event: CdkDragDrop<ToDo[]>) {
     console.log($event);
@@ -51,5 +87,12 @@ export class BoardComponent {
     } else {
       transferArrayItem($event.previousContainer.data, $event.container.data, $event.previousIndex, $event.currentIndex)
     }
+  }
+
+  addColumn() {
+    this.columns.push({
+      title: 'New columns',
+      todos: []
+    })
   }
 }
